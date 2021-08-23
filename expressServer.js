533f0cf6fa76ee878c7080abb3d50db10a9e3c54
Kayli Garwood <kgarwood@uncc.edu>
2021-08-23 16:46:33 -0400

    Edited NodeJS Express Server

--- expressServer.js
+++ expressServer.js
@@ -3,11 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
     res.send('Hello World!!!');
-=======
-    res.send('Hello World!');
->>>>>>> 2398c27251e1f132bb8a668c13ef38099e6e54cb
 });
 
 app.listen(port, () => {
